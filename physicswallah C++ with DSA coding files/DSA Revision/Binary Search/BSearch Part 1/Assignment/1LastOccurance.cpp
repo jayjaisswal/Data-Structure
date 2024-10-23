6ff2de60d096426d79ed2ae8d64f8d68b49517b0
jay kumar <kumarprince13833@gmail.com>
2024-10-24 00:44:16 +0530

    binary search

---
+++ physicswallah C++ with DSA coding files/DSA Revision/Binary Search/BSearch Part 1/Assignment/1LastOccurance.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1,4,4,4,5,5,6,6,7,7,8};                         
+    int size = sizeof(arr) / sizeof(arr[0]); 
+    int x = 6;                              
+
+    
+    if (size == 0)
+    {
+        cout << "Element is not found";
+        return 0;
+    }
+
+    bool flag = false;
+    int low = 0;
+    int high = size - 1;
+
+    while (high >= low)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == x)
+        {
+
+            if (mid == size - 1 || arr[mid] != arr[mid + 1])
+            {
+                flag = true;
+                cout << mid;
+                break;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else if (arr[mid] > x)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+    if (!flag)
+    {
+        cout << "Element is not found";
+    }
+
+    return 0;
+}
